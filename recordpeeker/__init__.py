d40550e352c7017010be3e880c5da7585439ea3b
Jonathan Chang <jchang641@gmail.com>
2015-05-01 17:51:09 -0700

    Add equipment optimizer

--- recordpeeker/__init__.py
+++ recordpeeker/__init__.py
@@ -1,1 +1,33 @@
+import csv
+from pkg_resources import resource_stream
+import heapq
+
 import command_line
+
+class Equipment(dict):
+    def __init__(self, sourcedict):
+        dict.__init__(self, **sourcedict)
+
+    def rs(self, series):
+        if self["series_id"] == int(series):
+            return {"name": self["name"], "acc": self["series_acc"], "atk": self["series_atk"],
+                    "def": self["series_def"], "eva": self["series_eva"], "matk": self["series_matk"],
+                    "mdef":self["series_mdef"], "mnd":self["series_mnd"]}
+        else:
+            return slicedict(self, "name acc atk def eva matk mdef mnd".split())
+
+def slicedict(d, s):
+    return {k:v for k,v in d.iteritems() if k in s}
+
+def best_equipment(series, heap, stat, n=3):
+    return [y.rs(series) for y in heapq.nlargest(n, heap, lambda x: x.rs(series)[stat])]
+
+def load_item_dict():
+    res = dict()
+    rfile = resource_stream("recordpeeker", "data/items.csv")
+    reader = csv.reader(rfile)
+    for row in reader:
+        res[row[0]] = row[1]
+    return res
+
+ITEMS = load_item_dict()
--- recordpeeker/mitmdump_input.py
+++ recordpeeker/mitmdump_input.py
@@ -1,18 +1,12 @@
 import json
-import csv
 from ast import literal_eval
-from pkg_resources import Requirement, resource_stream
 import socket
+import heapq
+from collections import OrderedDict
 
 from libmproxy.protocol.http import decoded
 
-def load_item_dict():
-    res = dict()
-    rfile = resource_stream("recordpeeker", "data/items.csv")
-    reader = csv.reader(rfile)
-    for row in reader:
-        res[row[0]] = row[1]
-    return res
+from recordpeeker import Equipment, ITEMS, slicedict, best_equipment
 
 def get_display_name(enemy):
     for child in enemy["children"]:
@@ -26,7 +20,6 @@ def get_drops(enemy):
 
 
 def handle_get_battle_init_data(data):
-    items = load_item_dict()
     print "Entering battle #{battle_id}..".format(**data["battle"])
     all_rounds_data = data['battle']['rounds']
     for round_data in all_rounds_data:
@@ -37,13 +30,33 @@ def handle_get_battle_init_data(data):
             for drop in get_drops(enemy):
                 if "item_id" in drop:
                     kind = "orb id#" if drop["type"] == 51 else "equipment id#"
-                    item = items.get(drop["item_id"], kind + drop["item_id"])
+                    item = ITEMS.get(drop["item_id"], kind + drop["item_id"])
                     print "    {0} drops {rarity}* {1}".format(name, item, **drop)
                 else:
                     print "    {0} drops {amount} gold".format(name, **drop)
                 had_drop = True
             if not had_drop:
                 print "    {0} drops nothing!".format(name)
+    print ""
+
+def handle_party_list(data):
+    wanted = "name series_id acc atk def eva matk mdef mnd series_acc series_atk series_def series_eva series_matk series_mdef series_mnd"
+    topn = OrderedDict()
+    topn["atk"] = 5
+    topn["matk"] = 2
+    topn["mnd"] = 2
+    topn["def"] = 5
+    find_series = [101001, 102001, 104001, 105001, 106001, 107001, 110001]
+    heap = []
+    for item in data["equipments"]:
+        heapq.heappush(heap, Equipment(slicedict(item, wanted)))
+    for series in find_series:
+        print "Best equipment for FF{0}:".format((series - 100001) / 1000)
+        for stat, count in topn.iteritems():
+            for equip in best_equipment(series, heap, stat, count):
+                name = equip["name"].replace(u"\uff0b", "+")
+                print "  {0}: {1} -- {2}".format(stat, equip[stat], name)
+    print ""
 
 def start(context, argv):
     global conf
@@ -52,7 +65,9 @@ def start(context, argv):
     ip = "" if ip == '127.0.0.1' else ip + ", "
     print "Configure your phone's proxy to point to this computer, then visit mitm.it"
     print "on your phone to install the interception certificate.\n"
-    print "Record Peeker is listening on {0}port {port}...".format(ip, **conf)
+    print "Record Peeker is listening on {0}port {port}.\n".format(ip, **conf)
+    print "Try entering the Party screen, or starting a battle."
+
 
 
 def response(context, flow):
@@ -61,3 +76,6 @@ def response(context, flow):
             if 'get_battle_init_data' in flow.request.path:
                 data = json.loads(flow.response.content)
                 handle_get_battle_init_data(data)
+            elif 'party/list' in flow.request.path:
+                data = json.loads(flow.response.content)
+                handle_party_list(data)
