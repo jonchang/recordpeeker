33dddef2408cae366965a8dfd6c6618226bbe2a0
= <jchang641@gmail.com>
2015-05-03 01:38:05 -0700

    Add help for verbosity

--- recordpeeker/command_line.py
+++ recordpeeker/command_line.py
@@ -6,7 +6,7 @@ import sys
 def parse_args(argv):
     parser = argparse.ArgumentParser("Test")
     parser.add_argument("--port", "-p", type=int, default=8080, help="Specify the port recordpeeker runs on")
-    parser.add_argument("--verbosity", "-v", default=0, type=int, choices=[0,1,2,3])
+    parser.add_argument("--verbosity", "-v", default=0, type=int, choices=[0,1,2,3], help="Spews more info. 1: prints the path of each request. 2: prints the content of unknown requests. 3: Also print the content of known requests.")
     return parser.parse_args(argv[1:])
 
 def launch():
